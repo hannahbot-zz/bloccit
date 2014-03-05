dae8870380e35192578b2a529dd5b65ac3111625
Hannah Bottalla <hannah@instructure.com>
2014-03-05 16:39:34 -0700

    Implemented ranking

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305040301) do
+ActiveRecord::Schema.define(version: 20140305231913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140305040301) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.string   "image"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
