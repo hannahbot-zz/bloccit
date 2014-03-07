88ab0f5e6711feb67542dc0598a1c32b340c4a68
Hannah Bottalla <hannah@instructure.com>
2014-03-06 21:11:22 -0700

    Created Popular Posts and Users, and optimized topics#show with eager loading

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,17 @@ class User < ActiveRecord::Base
     self.favorites.where(post_id: post.id).first
   end
 
+  def self.top_rated
+    self.select('users.*').
+        select('COUNT(DISTINCT comments.id) AS comments_count').
+        select('COUNT(DISTINCT posts.id) AS posts_count').
+        select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank').
+        joins(:posts).
+        joins(:comments).
+        group('users.id').
+        order('rank DESC')
+  end
+
   ROLES = %w[member moderator admin]
   def role?(base_role)
     role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
