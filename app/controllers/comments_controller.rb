a9a23eae252aa8af76c656f24f25dfe78b963584
Hannah Bottalla <hannah@instructure.com>
2014-03-09 20:13:52 -0600

    Added ajax for deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
 
   def create
     @topic = Topic.find(params[:topic_id])
@@ -23,11 +24,15 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
 
     authorize! :destroy, @comment, message: "You need to be own the comment to do that."
+
     if @comment.destroy
-      redirect_to [@topic, @post], notice: "Comment was deleted."
+      flash[:notice] = "Comment was removed"
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render 'posts/show'
+    end  
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post]}
     end
   end
 
